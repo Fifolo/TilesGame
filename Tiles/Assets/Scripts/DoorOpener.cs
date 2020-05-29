e89a26206716b94da84e7e6aaf91dd354c60c04c
Filip Starczewski <filip.starczewskii@gmail.com>
2020-05-29 13:32:06 +0200

    upload to remote repo

---
+++ Tiles/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer door;
+    public Sprite openedDoor;
+    public Sprite clickedLever;
+
+    private SpriteRenderer toggle;
+    void Start()
+    {
+        toggle = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            toggle.sprite = clickedLever;
+            door.sprite = openedDoor;
+        }
+    }
+}
---
+++ Tiles/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    public float playerSpeed = 1;
+    public float jumpHeight = 1;
+
+    private float horizontalDirection;
+    private float verticalDirection;
+    private Rigidbody2D rb;
+
+    private bool jumpClick = false;
+    private bool ground = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalDirection = Input.GetAxisRaw("Horizontal");
+        verticalDirection = Input.GetAxisRaw("Vertical");
+        //transform.position += new Vector3(horizontalDirection, verticalDirection, 0) * Time.deltaTime * playerSpeed; 
+        if (Input.GetButtonDown("Jump") && ground)
+        {
+            jumpClick = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalDirection * playerSpeed, rb.velocity.y);
+
+        if (jumpClick)
+        {
+            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            jumpClick = false;
+            ground = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Box"))
+        {
+            ground = true;
+        }
+    }
+}
